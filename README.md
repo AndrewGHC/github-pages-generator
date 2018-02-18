46650b12bbb519f35ab24a783f4c7bdd6ea9af73
AndrewGHC <drewwalsh1@gmail.com>
2018-02-18 21:58:44 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# github-pages-generator
+
+A setup for creating documentation that can be published to Github pages. Creating a React app for this purpose is tedious, and this is a reference for doing so.
+
+`npm run build` will take the app in ./example and compile it into a single index.html file in ./docs. Javascript assets are included inline.
+
+`npm run dev` serves the app locally with hot reloading using webpack-dev-server on the default port.
---
+++ example/webpack.config.js
@@ -0,0 +1,39 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import HtmlWebpackInlineSourcePlugin from 'html-webpack-inline-source-plugin';
+import FilterChunkWebpackPlugin from 'filter-chunk-webpack-plugin';
+import path from 'path';
+
+export default {
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  context: path.resolve(__dirname),
+  entry: './index',
+  output: {
+    path: `${__dirname}/../docs`,
+    publicPath: '/',
+    filename: '[name].[hash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.template.ejs',
+      inject: true,
+      inlineSource: '.js$',
+    }),
+    new HtmlWebpackInlineSourcePlugin(),
+    new FilterChunkWebpackPlugin({
+      patterns: [
+        '*.js',
+      ],
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+      },
+    ],
+  },
+};
